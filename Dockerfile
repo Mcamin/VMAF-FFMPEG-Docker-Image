8dbb8c416fa0ebe58dddf9f5126b2a5acbed378a
Ahmed Amine Mchayaa <ahmed.amine.mchayaa@fokus.fraunhofer.de>
2022-06-08 20:33:20 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,108 @@
+# Custom ffmpeg Dockerfile
+# https://github.com/five82/ffmpeg-git
+# https://github.com/jrottenberg/ffmpeg
+# https://github.com/sitkevij/ffmpeg/
+# Read Ubuntu https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
+# Versions:
+
+# ffmpeg     - git master HEAD
+# libvmaf    - git master HEAD
+# libzimg    - git master HEAD
+# libopus    - git master HEAD
+# libx264    - git master HEAD
+# libx265    - git master HEAD
+# libsvtav1  - git master HEAD
+# libaom     - git master HEAD
+# fdk-aac    - git master HEAD
+# libmp3lame - v3.100
+# libvorbis  - v1.3.5
+# libvpx     - v1.8.0
+
+# Use Debian for our base image
+FROM docker.io/debian:stable-slim AS build
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Set environemet variables
+ENV SRC=/usr/local \
+    LD_LIBRARY_PATH=${SRC}/lib \
+    PKG_CONFIG_PATH=${SRC}/lib/pkgconfig \
+    BIN=${SRC}/bin
+#--------------------------------
+# Update and install dependencies
+#--------------------------------
+# No, we're not going to version every apt package dependency.
+# That's a bad idea in practice and will cause problems.
+
+
+
+RUN \
+buildDeps="autoconf \
+    automake \
+    build-essential \
+    ca-certificates \
+    cmake \
+    doxygen \
+    ninja-build \
+    pkg-config \
+    python3-pip \
+    python3-setuptools \
+    python3-wheel \
+    texinfo \
+    git-core \
+    nasm \
+    yasm" && \
+export MAKEFLAGS="-j$(($(nproc) + 1))" && \
+apt-get -yqq update && \
+apt-get install -yq \
+--no-install-recommends ${buildDeps} && \
+ffmpegDeps="libasound2 \
+    libass-dev \
+    libfreetype6-dev \
+    libnuma-dev \
+    libtool-bin \
+    libsdl2-dev \
+    libtool \
+    libva-dev \
+    libvdpau-dev \
+    libvorbis-dev \
+    libxcb1-dev \
+    libxcb-shm0-dev \
+    libxcb-xfixes0-dev \
+    python3 \
+    zlib1g-dev" && \
+apt-get install -yq \
+--no-install-recommends ${ffmpegDeps} && \
+#--------------
+# Install meson
+#--------------
+pip3 install --no-cache-dir meson==0.57.1 && \
+#------------------
+# Setup directories
+#------------------
+mkdir -p /input /output /ffmpeg/ffmpeg_sources
+#-------------
+# Build ffmpeg
+#-------------
+RUN ./build_ffmpeg_vmaf.sh
+#----------------------------------------------------
+# Clean up directories and packages after compilation
+#----------------------------------------------------
+RUN pip3 uninstall meson -y && \
+apt-get purge -y ${BUILD_DEP} && \
+apt-get autoremove -y && \
+apt-get install -y \
+  --no-install-recommends \
+  libsdl2-dev && \
+apt-get clean && \
+apt-get autoclean && \
+rm -rf /var/lib/apt/lists/* && \
+rm -rf /ffmpeg
+#---------------------------------------
+# Run ffmpeg when the container launches
+#---------------------------------------
+CMD ["ffmpeg"]
---
+++ build_ffmpeg_vmaf.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# Compile and install ffmpeg.
+# Environment setup and packages dependencies are handled by the Dockerfile.
+
+#----------------
+# Download source
+#----------------
+cd /ffmpeg/ffmpeg_sources || exit
+mkdir {libmp3lame,libvorbis,libvpx}
+git clone https://github.com/sekrit-twc/zimg.git zimg
+git clone https://github.com/Netflix/vmaf.git vmaf
+git clone --depth 1 https://github.com/xiph/opus.git opus
+git clone --depth 1 https://code.videolan.org/videolan/x264.git x264
+git clone https://github.com/videolan/x265.git x265
+git clone https://github.com/AOMediaCodec/SVT-AV1.git SVT-AV1
+git clone https://aomedia.googlesource.com/aom aom
+git clone https://github.com/FFmpeg/FFmpeg ffmpeg
+git clone https://github.com/mstorsjo/fdk-aac fdk-acc
+wget -P /ffmpeg/ffmpeg_sources/libmp3lame https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz/download
+wget -P /ffmpeg/ffmpeg_sources/libvorbis  http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+wget -P /ffmpeg/ffmpeg_sources/libvpx https://codeload.github.com/webmproject/libvpx/tar.gz/v1.8.0
+
+#-------------------
+# Compile z.lib/zimg
+#-------------------
+cd /ffmpeg/ffmpeg_sources/zimg || exit
+./autogen.sh
+./configure
+make && \
+make install
+
+#----------------
+# Compile libvmaf
+#----------------
+cd /ffmpeg/ffmpeg_sources/vmaf/libvmaf || exit
+meson build --buildtype release
+ninja -vC build
+ninja -vC build install
+mkdir -p /usr/local/share/model/
+cp -r /ffmpeg/ffmpeg_sources/vmaf/model/* /usr/local/share/model/
+
+#----------------
+# Compile libopus
+#----------------
+cd /ffmpeg/ffmpeg_sources/opus || exit
+./autogen.sh
+./configure
+make && \
+make install
+
+#------------------
+# Compile libsvtav1
+#------------------
+cd /ffmpeg/ffmpeg_sources/SVT-AV1/Build || exit
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+make && \
+make install
+
+#----------------
+# Compile libx264
+#----------------
+cd /ffmpeg/ffmpeg_sources/x264 || exit
+./configure \
+  --enable-static \
+  --enable-pic
+make && \
+make install && \
+make distclean
+
+#----------------
+# Compile libx265
+#----------------
+cd /ffmpeg/ffmpeg_sources/x265/build/linux || exit
+cmake -G "Unix Makefiles" \
+  -DHIGH_BIT_DEPTH=on \
+  -DENABLE_CLI=OFF \
+  ../../source && \
+make && \
+make install && \
+make clean
+
+#---------------
+# Compile libaom
+#---------------
+cd /ffmpeg/ffmpeg_sources/aom || exit
+mkdir -p ../aom_build
+cd ../aom_build || exit
+cmake /ffmpeg/ffmpeg_sources/aom -DBUILD_SHARED_LIBS=1 && \
+make && \
+make install
+
+#---------------
+# Compile fdk-aac
+#---------------
+cd /ffmpeg/ffmpeg_sources/fdk-aac || exit
+autoreconf -fiv && \
+./configure && \
+make && \
+make install
+
+#---------------
+# Compile libmp3lame
+#---------------
+cd /ffmpeg/ffmpeg_sources/libmp3lame || exit
+tar -zx --strip-components=1 && \
+./configure --enable-nasm --disable-frontend && \
+make && \
+make install
+
+# Compile libvorbis
+#---------------
+cd /ffmpeg/ffmpeg_sources/libvorbis || exit
+tar -zx --strip-components=1 -f libvorbis-1.3.5.tar.gz && \
+./configure && \
+make && \
+make install
+
+# Compile libvpx
+#---------------
+cd /ffmpeg/ffmpeg_sources/libvpx || exit
+tar -zx --strip-components=1 && \
+./configure --enable-vp8 --enable-vp9 --enable-vp9-highbitdepth --enable-pic \
+--disable-debug --disable-examples --disable-docs --disable-install-bins && \
+make && \
+make install
+
+#---------------
+# Compile ffmpeg
+#---------------
+cd /ffmpeg/ffmpeg_sources/ffmpeg || exit
+./configure --prefix="${SRC}" --extra-cflags="-I${SRC}/include" --pkg-config-flags="--static" --extra-ldflags="-L${SRC}/lib" --bindir="${SRC}/bin" \
+  --disable-static \
+  --disable-debug \
+  --disable-doc \
+  --disable-ffplay \
+  --enable-shared \
+  --enable-libfreetype \
+  --enable-libzimg \
+  --enable-libsvtav1 \
+  --enable-postproc \
+  --enable-small \
+  # FAME Packages
+  --enable-gpl \
+  --enable-libaom \
+  --enable-libfdk-aac \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-nonfree \
+  --enable-libvmaf \
+  --enable-version3
+make -j "$(nproc)"
+make install
+hash -r
